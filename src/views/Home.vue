fd891310a8f3490f4e320b544d71427b8eec1e38
Deepak Bhardwaj <kapeed07@gmail.com>
2020-05-14 18:36:00 +0530

    initial boilerplate with tailwind, axios, loadsh

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="mt-5 flex items-center justify-center">
+    <p class="text-red-500">Hello World</p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld,
+  props: [],
+  data() {
+    return {};
   },
 };
 </script>
+
+<style scoped>
+
+</style>
